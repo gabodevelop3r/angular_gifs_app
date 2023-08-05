df0e0638809701a88df69acead3f55bda23d3ee1
Gabo-dev <gabrielgallegos@MacBook-Pro-2.local>
2023-08-05 01:14:22 -0400

    Gifs WIP

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  private _tagsHistory: string[] = [];
+
+  constructor() { }
+
+
+  get tagsHistory(): string[] {
+
+    return [ ...this._tagsHistory ]
+
+  }
+
+  searchTag( tag: string ):void {
+
+    if( tag.length === 0 ) return ;
+
+    this.organizeHistory( tag );
+
+  }
+
+
+  private organizeHistory( tag: string ): void {
+
+    tag = tag.toLowerCase();
+
+    if (this._tagsHistory.includes(tag))
+      this._tagsHistory = this._tagsHistory.filter( oldTag =>  oldTag !== tag)
+
+    this._tagsHistory.unshift( tag )
+
+    this._tagsHistory = this._tagsHistory.splice( 0, 10 )
+
+  }
+
+
+
+}
